fea2066340f5d848f5808225cce2443e94caf032
suxing.zhang <suxing.zhang@pousheng.com>
2019-04-28 09:18:06 +0800

    引入lombok

---
+++ src/main/java/com/spring/cloud/domain/controller/dto/TestDto.java
@@ -0,0 +1,17 @@
+/*
+ * All rights Reserved, Designed By baowei
+ *
+ * 注意：本内容仅限于内部传阅，禁止外泄以及用于其他的商业目的
+ */
+
+package com.spring.cloud.domain.controller.dto;
+
+import lombok.Data;
+
+/**
+ * @author suxing.zhang
+ * @since 2019/4/26
+ */
+@Data
+public class TestDto {
+}
