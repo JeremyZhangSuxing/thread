3c58dd5c42f24b374d20de8fabecb725eb24a1ec
suxing.zhang <suxing.zhang@pousheng.com>
2020-03-04 16:31:34 +0800

    动态代理代码修改

--- src/main/java/com/spring/cloud/cglib/TestCglib.java
+++ src/main/java/com/spring/cloud/cglib/TestCglib.java
@@ -9,6 +9,11 @@ import org.springframework.cglib.proxy.Enhancer;
  **/
 public class TestCglib {
     public static void main(String[] args) {
+        /**
+         * cglib 动态代理  目标对象的子类作为代理对象
+         * MethodInterceptor 实现callback子接口对目标对象的方法进行改装
+         *
+         */
         Enhancer enhancer = new Enhancer();
         enhancer.setSuperclass(Student.class);
         Callback callback = new CallMethod();
--- src/main/java/com/spring/cloud/proxy/MyInvocationHandle.java
+++ src/main/java/com/spring/cloud/proxy/MyInvocationHandle.java
@@ -1,5 +1,8 @@
 package com.spring.cloud.proxy;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
@@ -7,20 +10,18 @@ import java.lang.reflect.Method;
  * @author zhang.suxing
  * @date 2020/3/3 11:17
  **/
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class MyInvocationHandle implements InvocationHandler {
-
+    /**
+     * 目标对象传递进来
+     */
     private Object targetObject;
-
-    public MyInvocationHandle(Object targetObject) {
-        this.targetObject = targetObject;
-    }
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         if (method.getName().equalsIgnoreCase("add")) {
             return method.invoke(targetObject);
         }
-
         System.out.println("----代理目标对象目标方法之前的执行---");
         Object returnResult = method.invoke(targetObject, args);
         System.out.println("----代理目标对象目标方法之后的执行---");
