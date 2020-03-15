bb76e51d68738c4f40589f0a1d6df4d583126412
suxing.zhang <suxing.zhang@pousheng.com>
2020-03-15 12:00:57 +0800

    动态代理代码修改

--- src/main/java/com/spring/cloud/proxy/TestProxy.java
+++ src/main/java/com/spring/cloud/proxy/TestProxy.java
@@ -10,7 +10,10 @@ public class TestProxy {
     public static void main(String[] args) {
         UserDao userDao = new UserDaoImpl();
         /**
-         * 类加载器  目标对象的所有接口 通过类字节码获取
+         * 1.类加载器  (class的字节码对象获取)
+         * 2.目标对象的所有接口  (class的字节码对象获取)
+         * 3.需要以恶搞实现 InvocationHandle接口的对象 对目标对象方法的增强在这哥对象中完成
+         *
          */
         Object proxyInstance = Proxy.newProxyInstance(userDao.getClass().getClassLoader(), userDao.getClass().getInterfaces(), new MyInvocationHandle(userDao));
         UserDao userDaoProxy = (UserDao) proxyInstance;
