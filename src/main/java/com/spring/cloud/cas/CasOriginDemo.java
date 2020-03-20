c09987c9a146f84f927e351957171d0dbb59f068
suxing.zhang <suxing.zhang@pousheng.com>
2020-03-20 09:33:40 +0800

    cas模拟

--- src/main/java/com/spring/cloud/cas/CasOriginDemo.java
+++ src/main/java/com/spring/cloud/cas/CasOriginDemo.java
@@ -1,24 +1,86 @@
 package com.spring.cloud.cas;
 
+import com.spring.cloud.util.ThreadUtils;
+
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicStampedReference;
 
 /**
  * @author zhang.suxing
  * @date 2020/3/19 13:59
  **/
 public class CasOriginDemo {
-    public static void main(String[] args) {
-        AtomicInteger atomicInteger = new AtomicInteger(0);
 
-        Thread mainThread = new Thread(() -> {
-            try {
-                int expect = atomicInteger.get();
-                Thread.sleep(1000L);
-//                boolean result = atomicInteger.compareAndSet(expect)
-                System.out.println(Thread.currentThread().getName() + "当前值 = " + atomicInteger.get());
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
+    public static void main(String[] args) {
+        ExecutorService executorService = ThreadUtils.buildThreadPool();
+        /**
+         * 测试abd
+         */
+//        executorService.execute(CasOriginDemo::mainThread);
+//        executorService.execute(CasOriginDemo::otherThread);
+        /**
+         * 测试版版本号避免ada
+         */
+        executorService.execute(CasOriginDemo::mainReferenceThread);
+        executorService.execute(CasOriginDemo::otherReferenceThread);
+    }
+
+    private static AtomicInteger atomicInteger = new AtomicInteger(0);
+    private static AtomicStampedReference<Integer> atomicReference = new AtomicStampedReference<>(1, 1);
+
+    private static void mainThread() {
+        try {
+            int expect = atomicInteger.get();
+            System.out.println("操作线程---" + Thread.currentThread().getName() + "当前值 = " + atomicInteger.get());
+            int newCount = expect + 1;
+            Thread.sleep(1000L);
+            boolean result = atomicInteger.compareAndSet(expect, newCount);
+            System.out.println("操作线程---" + Thread.currentThread().getName() + "当前值 = " + atomicInteger.get() + "【cas操作】" + result);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void otherThread() {
+        try {
+            Thread.sleep(20);
+            atomicInteger.incrementAndGet();
+            System.out.println("操作线程---" + Thread.currentThread().getName() + "increment   当前值 = " + atomicInteger.get());
+            atomicInteger.decrementAndGet();
+            System.out.println("操作线程---" + Thread.currentThread().getName() + "decrement    当前值 = " + atomicInteger.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void mainReferenceThread() {
+        try {
+            int expect = atomicReference.getReference();
+            int newCount = expect + 1;
+            int expectStamp = atomicReference.getStamp();
+            int newStamp = expectStamp + 1;
+            System.out.println("操作线程---mainReferenceThread" + "当前值 = " + expect + "【当前版本号】" + expectStamp);
+            Thread.sleep(1000L);
+            atomicReference.compareAndSet(expect, newCount, expectStamp, newStamp);
+            boolean result = atomicInteger.compareAndSet(expect, newCount);
+            System.out.println("操作线程---mainReferenceThread" + "当前值 = " + atomicInteger.get() + "【cas操作】" + result);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private static void otherReferenceThread() {
+        try {
+            Thread.sleep(20);
+            atomicReference.compareAndSet(atomicReference.getReference(), atomicReference.getReference() + 1, atomicReference.getStamp(), atomicReference.getStamp() + 1);
+            System.out.println("操作线程---otherReferenceThread" + "当前值 = " + atomicReference.getReference() + "【当前版本号】" + atomicReference.getStamp());
+            atomicReference.compareAndSet(atomicReference.getReference(), atomicReference.getReference() - 1, atomicReference.getStamp(), atomicReference.getStamp() + 1);
+            System.out.println("操作线程---otherReferenceThread" + "当前值 = " + atomicReference.getReference() + "【当前版本号】" + atomicReference.getStamp());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
+
