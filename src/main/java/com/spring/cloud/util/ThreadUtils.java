12d110510409efa50ae295fec1aa511b5631b05f
suxing.zhang <suxing.zhang@pousheng.com>
2020-03-20 09:33:57 +0800

    cas模拟

---
+++ src/main/java/com/spring/cloud/util/ThreadUtils.java
@@ -0,0 +1,23 @@
+package com.spring.cloud.util;
+
+import org.springframework.scheduling.concurrent.CustomizableThreadFactory;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author zhang.suxing
+ * @date 2020/3/19 21:23
+ **/
+public class ThreadUtils {
+
+    public static ExecutorService buildThreadPool() {
+        return new ThreadPoolExecutor(
+                10, 50, 3, TimeUnit.MINUTES,
+                new ArrayBlockingQueue<>(5000),
+                new CustomizableThreadFactory("user_common_thread-"),
+                new ThreadPoolExecutor.CallerRunsPolicy());
+    }
+}
